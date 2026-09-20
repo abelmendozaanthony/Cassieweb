c92d2580f744793dde3fc9e95930e55024781e29
Anthonny Abel <anthonyabelmendoza123@gmail.com>
2026-09-19 20:35:10 -0500

    Corregir ALLOWED_HOSTS

--- cassie/settings.py
+++ cassie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_rfc$$evg3q&hmck@(rk+s)jmv4g&rq=+bj#4nmkubcgndta5y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cassieweb.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
